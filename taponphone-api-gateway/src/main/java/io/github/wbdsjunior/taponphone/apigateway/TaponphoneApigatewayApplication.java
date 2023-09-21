85fe30abe98c99e2aada9d65ea80a65be9481384
Wbdsjunior <wbdsjunior@gmail.com>
2023-09-21 20:49:38 -0300

    refactoring...

---
+++ taponphone-api-gateway/src/main/java/io/github/wbdsjunior/taponphone/apigateway/TaponphoneApigatewayApplication.java
@@ -0,0 +1,16 @@
+package io.github.wbdsjunior.taponphone.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaponphoneApigatewayApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(
+                  TaponphoneApigatewayApplication.class
+                , args
+            );
+    }
+}
